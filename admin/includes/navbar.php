e3d834a6e88a7d1bb21706fea8c461868b5d9f7d
Steven-loki <sanjay.i@taffinc.com>
2024-01-31 13:27:26 +0530

    day-2

---
+++ admin/includes/navbar.php
@@ -0,0 +1,26 @@
+<!------- navbar ------------------>
+
+<header class="main-header">
+    <a href="home.php" class="logo">
+
+        <!--------mini logo for sidebar ----->
+        <span class="logo-mini"><b>A</b>system</span>
+
+        <!-----logo for regular mobile device --->
+        <span class="logo-lg"><b>Attendacnce</b> system</span>
+    </a>
+
+    <!--------header navbar: style header less ---->
+    <nav class="navbar navbar-static-top">
+        <!------sidebar toggle button ----->
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="botton">
+            <span class="sr-only">Toggle navigation</span>
+
+        </a>
+
+        <div class="nav></div>
+
+
+
+
+</header>
