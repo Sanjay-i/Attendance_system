51a502c2196d7491ed7511827c945a35ffa8d91a
Steven-loki <sanjay.i@taffinc.com>
2024-02-01 16:07:37 +0530

    dashboard

--- dashboard.php
+++ dashboard.php
@@ -12,15 +12,18 @@ if (isset($_POST['submit'])) {
     $result = mysqli_query($data, "insert into user value('','$name','$email','$pass','$num')");
 
     if ($result) {
-        $query = "SELECT Email,Password,Name FROM user WHERE Email='$email' and Password='$pass' and Name='$name'";
+        $query = "SELECT ID, Email,Password,Name FROM user WHERE Email='$email' and Password='$pass' and Name='$name'";
         $result1 = mysqli_query($data, $query);
         if (mysqli_num_rows($result1) > 0) {
             $row = mysqli_fetch_assoc($result1);
+            $_SESSION['user_id'] = $row['ID'];
             $_SESSION['user_email'] = $row['Email'];
             $_SESSION['user_name'] = $row['Name'];
 
-            print_r($_SESSION);
-            echo 'successfuly logged';
+            // print_r($_SESSION);
+            // echo 'successfuly logged';
+
+            header('location:view.php');
         }
     } else {
         echo "somthing wrong";
