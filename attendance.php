33cd20c68fe0eff22db22e330833aceb5831eca8
Steven-loki <sanjay.i@taffinc.com>
2024-02-02 19:26:20 +0530

    I have created an attendance page that allows users to check in and check out. When a user clicks the ‘check in’ button, the current time is stored in the database. Similarly, when the user clicks the ‘check out’ button, the current time is stored in the database as well.

---
+++ attendance.php
@@ -0,0 +1,47 @@
+
+<?php
+date_default_timezone_set('Asia/Kolkata');
+
+include('conn.php');
+
+$result;
+if (isset($_POST['userId'])) {
+    $userId = $_POST['userId'];
+    $checkIn = $_POST['checkIn'];
+    $currentDateTime = date('Y-m-d H:i:s');
+    if ($checkIn == 'true') {
+        $query = "SELECT userId FROM attendance WHERE userId = '$userId' AND checkIn != '0000-00-00 00:00:00'";
+        $result1 = mysqli_query($data, $query);
+
+        if (mysqli_num_rows($result1) > 0) {
+            $result = array(
+                "status" => false,
+                "message" => 'Already checked in'
+            );
+        } else {
+            $rquery = mysqli_query($data, "INSERT INTO attendance(userId, checkIn) VALUES('$userId','$currentDateTime')");
+            $result = array(
+                "status" => true,
+                "message" => 'CheckIn Successfully'
+            );
+        }
+    } else {
+        $query = "SELECT userId FROM attendance WHERE userId = '$userId' AND checkOut != '0000-00-00 00:00:00'";
+        $result1 = mysqli_query($data, $query);
+
+        if (mysqli_num_rows($result1) > 0) {
+            $result = array(
+                "status" => false,
+                "message" => 'Already checked out'
+            );
+        } else {
+            $rquery = mysqli_query($data, "UPDATE attendance SET checkOut = '$currentDateTime' WHERE userId = '$userId' ");
+            $result = array(
+                "status" => true,
+                "message" => 'Checkout Successfully'
+            );
+        }
+    }
+}
+
+echo json_encode($result);
--- view.php
+++ view.php
@@ -1,14 +1,17 @@
 <?php
 session_start();
+
 if (isset($_SESSION['user_email']) && isset($_SESSION['user_name'])) {
 ?>
     <html>
 
     <head>
         <title>Dashboard</title>
-        <link href="css/mystyle.css" rel="stylesheet" type="text/css" />
-
+        <link href="css/style.css" rel="stylesheet" type="text/css" />
+        <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
     </head>
+    <!-------check in check-out page----->
 
     <body>
         <div class="container">
@@ -19,12 +22,59 @@ if (isset($_SESSION['user_email']) && isset($_SESSION['user_name'])) {
                     <div>ID:<?= $_SESSION['user_id'] ?></div>
                     <div>Name:<?= $_SESSION['user_name'] ?></div>
                     <div>Email:<?= $_SESSION['user_email'] ?> </div>
-                    <div><button class="check-in-btn">Check in </button></div>
-                    <div><button class="check-out-btn">Check out </button></div>
+                    <div><button onclick=checkIn() class="check-in-btn">Check in </button></div>
+                    <span id="time"> </span>
+                    <div><button onclick=checkOut() class="check-out-btn">Check out </button></div>
                 </div>
             </div>
         </div>
     </body>
+
+    <!------- add to id datetime check-in check-out ---->
+    <script>
+        function checkIn() {
+            let dateTime = new Date().toDateString();
+            $.ajax({
+                type: "POST",
+                url: "attendance.php",
+                data: {
+                    "userId": <?php echo $_SESSION['user_id']; ?>,
+                    "checkIn": true,
+                },
+                success: function(data) {
+                    let decodeData = JSON.parse(data);
+                    if (data.status == true) {
+                        alert("Checkin Successfully");
+                    } else {
+                        alert("already you checked in!!!");
+                    }
+                }
+            });
+        }
+
+        //<!---------check-out button onclick function ------>  
+
+        function checkOut() {
+            let dateTime = new Date().toDateString();
+            $.ajax({
+                type: "POST",
+                url: "attendance.php",
+                data: {
+                    "userId": <?php echo $_SESSION['user_id']; ?>,
+                    "checkIn": false
+                },
+                success: function(data) {
+                    let decodeData = JSON.parse(data);
+                    if (data.status == true) {
+                        alert("Checkout Successfully");
+                    } else {
+                        alert("already you checkout !!!");
+                    }
+                }
+            });
+
+        }
+    </script>
 
     </html>
 <?php
